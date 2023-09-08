0ed98345466119ebe6ae4bb7c5d7159179787fff
Dyrox <dyhmusicstuff@gmail.com>
2023-09-08 18:27:56 +0100

    code factoring & general debugging
    found the dictionary size change error, turns out there wasn't anything wrong with the code, its just the offgrid rendering thing is fucked and i didnt really know what was going on

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,25 +1,25 @@
-import pygame
 import os
+
+import pygame
+
 BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
-    img.set_colorkey((0,0,0))
+    img.set_colorkey((0, 0, 0))
     return img
 
 def load_images(path):
     images = []
-    image_names = os.listdir(BASE_IMG_PATH + path)
-    image_names.sort()  # Sort the image names
-    for img_name in image_names:
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(path + '/' + img_name))
     return images
 
 class Animation:
-    def __init__(self,images,img_dur=5,loop=True):
+    def __init__(self, images, img_dur=5, loop=True):
         self.images = images
-        self.img_duration = img_dur
         self.loop = loop
+        self.img_duration = img_dur
         self.done = False
         self.frame = 0
     
@@ -28,11 +28,11 @@ class Animation:
     
     def update(self):
         if self.loop:
-            self.frame = (self.frame + 1) % (len(self.images) * self.img_duration)
+            self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
         else:
-            self.frame = min(self.frame + 1, len(self.images) * self.img_duration - 1)
-            if self.frame >= len(self.images) * self.img_duration - 1:
+            self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
+            if self.frame >= self.img_duration * len(self.images) - 1:
                 self.done = True
-                
+    
     def img(self):
-        return self.images[int(self.frame/self.img_duration)]
+        return self.images[int(self.frame / self.img_duration)]
